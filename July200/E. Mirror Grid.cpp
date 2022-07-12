a850aa659f21d169ae93552caa09aeea896fc599
Carlos Raúl Romero Martínez <45341513+Batishavo@users.noreply.github.com>
2022-07-12 15:34:43 -0500

    Add files via upload

---
+++ July200/E. Mirror Grid.cpp
@@ -0,0 +1,85 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+string arr[150];
+
+int mat[150][150];
+int main(){
+
+    int test;
+    scanf("%d",&test);
+
+    while(test--){
+        int n;
+        int total=0;
+        char tp='a';
+        scanf("%d",&n);
+        for(int i=0;i<n;i++){
+
+            cin>>arr[i];
+
+            /*for(int j=0;j<n;j++){
+
+                scanf("%d",&arr[i][j]);
+            }*/
+        }
+
+        for(int i=0;i<n/2;i++){
+            for(int j=0;j<n/2;j++){
+                 int num= arr[i][j]+
+                     arr[i][n-j-1]+
+                     arr[n-i-1][j]+
+                     arr[n-i-1][n-j-1];
+                     mat[i][j]=tp;
+                     mat[i][n-j-1]=tp;
+                     mat[n-i-1][j]=tp;
+                     mat[n-i-1][n-j-1]=tp;
+                     tp++;
+
+                num-='0'*4;
+
+                if(num==3 || num==1){
+                    total++;
+                }
+                if(num==2){
+                    total+=2;
+                }
+
+            }
+        }
+          if(n%2!=0){
+            for(int i=0;i<n/2;i++){
+                int num=arr[i][n/2]+
+                        arr[n/2][i]+
+                        arr[n/2][n-i-1]+
+                        arr[n-i-1][n/2];
+                        mat[i][n/2]=tp;
+                        mat[n/2][i]=tp;
+                        mat[n/2][n-i-1]=tp;
+                        mat[n-i-1][n/2]=tp;
+                        tp++;
+                 num-='0'*4;
+                if(num==3 || num==1){
+                    total++;
+                }
+                if(num==2){
+                    total+=2;
+                }
+            }
+
+          }
+
+
+        printf("%d\n",total);
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                printf("%c ",mat[i][j]%10);
+            }
+            printf("\n");
+        }
+
+    }
+
+    return 0;
+}
