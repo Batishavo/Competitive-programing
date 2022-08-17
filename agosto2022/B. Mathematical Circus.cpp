75cc4952bb28e4f7930d6768eb246ccfdf1034d7
Carlos Raúl Romero Martínez <45341513+Batishavo@users.noreply.github.com>
2022-08-17 13:18:40 -0500

    Add files via upload

--- agosto2022/B. Mathematical Circus.cpp
+++ agosto2022/B. Mathematical Circus.cpp
@@ -4,12 +4,28 @@ using namespace std;
 
 int t,
     n,
-    k,
-    cont;
+    cont,
+    pos;
+
+long long k;
 
 vector<int> fours,
             sums,
             mores;
+
+void imprime(){
+    for(auto x: sums){
+
+        printf("%d %d\n",x ,mores[pos++]);
+
+    }
+    //printf("---\n");
+    for(auto x: fours){
+
+        printf("%d %d\n",mores[pos++] ,x );
+
+    }
+}
 
 int main(){
 
@@ -17,62 +33,108 @@ int main(){
 
     while(t--){
 
-        scanf("%d %d",&n,&k);
+        scanf("%d %lld",&n,&k);
 
 
-            cont=0;
-            int pos=0;
-            fours.clear();
-            sums.clear();
-            mores.clear();
+        cont=0;
+        pos=0;
+        fours.clear();
+        sums.clear();
+        mores.clear();
 
-            bool flag = false;
+        bool flag = false;
 
-            for(int i=1;i<=n;i++){
+        for(int i=1;i<=n;i++){
 
-                if(i%4==0 && !flag){
+            if(i%4==0 && !flag){
 
-                    fours.push_back(i);
-                    cont++;
-
-                }
-                else if((i+k)%4==0  && !flag){
-                    sums.push_back(i);
-                    cont++;
-                }
-                else{
-                    mores.push_back(i);
-                }
-                if(cont>=n/2){
-                   flag = true;
-                }
+                fours.push_back(i);
+                cont++;
 
             }
-
-            //cout<<cont<<endl;
-
-            if(flag){
-                 printf("YES\n");
-                for(auto x: sums){
-
-                    printf("%d %d\n",x ,mores[pos++]);
-
-                }
-                for(auto x: fours){
-
-                    printf("%d %d\n",mores[pos++] ,x );
-
-                }
+            else if((i+k)%4==0  && !flag){
+                sums.push_back(i);
+                cont++;
             }
             else{
-                 printf("NO\n");
+                mores.push_back(i);
             }
 
-
+            if(cont>=n/2){
+               flag = true;
+            }
 
         }
 
+            //cout<<cont<<endl;
+
+        if(flag){
+            printf("YES\n");
+            imprime();
+        }
+        else{
+            vector<int> dos,
+                        aux;
+
+            int bus=0;
+
+             for(auto x :  mores){
+
+                if(x%2==0 && cont<n/2 && bus!=2){
+                    dos.push_back(x);
+                    bus=2;
+                    if(dos.size()%2==0){
+                        cont++;
+                    }
+                }
+                else if((x+k)%2==0 && cont<n/2  && bus!=1){
+
+                    dos.push_back(x);
+                    bus=1;
+
+                    if(dos.size()%2==0){
+                        cont++;
+                    }
+
+                }
+                else{
+                    mores.push_back(x);
+                }
+
+             }
+             mores=aux;
+
+             //cont+=min(dos.size(),masdos.size());
+
+             //cout<<cont<<" "<<dos.size()<<" "<<masdos.size()<<endl;
+
+             if(cont>=n/2){
+                printf("YES\n");
+                while(pos<dos.size()){
+
+                    if(((dos[pos]+k)*dos[pos+1])%4==0){
+                      //printf("--%d %d",dos[pos],dos[pos+1]);
+                       printf("%d %d\n",dos[pos],dos[pos+1]);
+
+
+                    }
+                    else{
+                        printf("%d %d\n",dos[pos+1],dos[pos]);
+                    }
+                     pos+=2;
+
+                }
+                //printf("%d---\n",cont);
+                imprime();
+
+             }
+             else{
+                 printf("NO\n");
+             }
+
+        }
     }
+
 
     return 0;
 }
