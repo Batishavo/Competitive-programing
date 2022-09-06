b27726b45908700b46f1453f6b4244545416b522
Carlos Raúl Romero Martínez <45341513+Batishavo@users.noreply.github.com>
2022-09-06 11:57:15 -0500

    Add files via upload

--- septiembre2022/B. Madoka and Underground Competitions.cpp
+++ septiembre2022/B. Madoka and Underground Competitions.cpp
@@ -6,73 +6,11 @@ int n,k,r,c,t;
 
 char mat[505][505];
 
-void mark(){
+int pi[5]={0,0,0,0},
+    pj[5]={0,0,0,0};
 
-
-    for(int i=1;i<=n;i+=k){
-        for(int j=1;j<=n;j+=k){
-            int cont=j;
-            if(i+k>c && i<=c && j+k>t && j<=t){
-
-                //printf("---------------");
-                marSpecia(i,j);
-                continue;
-            }
-            for(int x=i;x<i+k;x++){
-                mat[x][cont]='X';
-                cont++;
-            }
-            /*for(int i=1;i<=n;i++){
-                for(int j=1;j<=n;j++){
-                    cout<<mat[i][j];
-                }
-                cout<<endl;
-            */
-
-        }
-    }
-
-}
-
-bool revisar(int i,int fi,int j,int fj){
-
-    for(int x=i;x<=fi;x++){
-        if(mat[x][j]=='X'){
-            return false;
-        }
-    }
-    for(int x=i;x<=fi;x--){
-        if(mat[x][j]=='X'){
-            return false;
-        }
-    }
-    for(int x=j;x<=fj;x++){
-        if(mat[i][x]=='X'){
-            return false;
-        }
-    }
-    for(int x=i;x<=fi;x++){
-        if(mat[x][j]=='X'){
-            return false;
-        }
-    }
-
-}
-
-void marSpecia(int i,int j){
-
-    int fi=i+k,
-        fj=j+k;
-
-    for(i;i<=i+k;i++){
-        for(j;j<=j+k;j++){
-
-            if(revisar(i,fi,j, fj)){
-                mat[i][j]='X'
-            }
-        }
-
-}
+queue<pair<int,int>>marcas;
+pair<int,int>actual;
 
 void clean(){
 
@@ -83,6 +21,67 @@ void clean(){
     }
 
 }
+bool putDot(int i,int j){
+
+    for(int x=i+1;x<i+k;x++){
+        if(x>n){
+            break;
+        }
+        if(mat[x][j]=='X'){
+            return false;
+        }
+    }
+    for(int x=i-1;x>i-k;x--){
+        if(x<1){
+            break;
+        }
+        if(mat[x][j]=='X'){
+            return false;
+        }
+    }
+    for(int x=j+1;x<j+k;x++){
+        if(x>n){
+            break;
+        }
+        if(mat[i][x]=='X'){
+            return false;
+        }
+    }
+    for(int x=j-1;x>j-k;x--){
+        if(x<1){
+            break;
+        }
+        if(mat[i][x]=='X'){
+            return false;
+        }
+    }
+    return true;
+}
+
+void bfs(){
+
+    while(!marcas.empty()){
+        actual=marcas.front();
+        marcas.pop();
+        int i=actual.first,
+            j=actual.second;
+        mat[i][j]='X';
+        //putDot(i,j);
+        for(int k=0;k<4;k++){
+
+            if(i+pi[k]>=1 &&  i+pi[k]<=n && j+pj[k]>=1 && j+pj[k]<=n){
+                if(mat[i+pi[k]][j+pj[k]]=='.'){
+                   marcas.push(make_pair(i+pi[k],j+pj[k]));
+                }
+            }
+
+        }
+
+
+    }
+
+
+}
 
 int main(){
 
@@ -90,12 +89,29 @@ int main(){
 
     while(t--){
 
-        scanf("%d %d %d %d",&n,&k,&c,&t);
-
-        mat[r][c]='X';
+        scanf("%d %d %d %d",&n,&k,&r,&c);
+        if(k==1){
+            for(int i=1;i<=n;i++){
+                for(int j=1;j<=n;j++){
+                    cout<<'X';
+                }
+            cout<<endl;
+            }
+            continue;
+        }
         clean();
-        mark();
-        cout<<"---"<<endl;
+        pi[0]=k;pi[2]=-k;
+        pj[1]=k;pj[3]=-k;
+        marcas.push(make_pair(r,c));
+        bfs();
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                if(mat[i][j]=='.' &&  putDot(i,j)){
+                    marcas.push(make_pair(i,j));
+                    bfs();
+                }
+            }
+        }
 
         for(int i=1;i<=n;i++){
             for(int j=1;j<=n;j++){
@@ -103,7 +119,7 @@ int main(){
             }
             cout<<endl;
         }
+       // printf("t:%d\n",t);
     }
     return 0;
 }
-
