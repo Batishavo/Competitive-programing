5407296b064fe7557b315db4b50e254e624df4e7
Carlos Raúl Romero Martínez <45341513+Batishavo@users.noreply.github.com>
2022-09-07 10:50:42 -0500

    Add files via upload

--- septiembre2022/B. Madoka and Underground Competitions.cpp
+++ septiembre2022/B. Madoka and Underground Competitions.cpp
@@ -5,13 +5,7 @@ using namespace std;
 int n,k,r,c,t;
 
 char mat[505][505];
-
-int pi[5]={0,0,0,0},
-    pj[5]={0,0,0,0};
-
-queue<pair<int,int>>marcas;
-pair<int,int>actual;
-
+//int cont=1;
 void clean(){
 
     for(int i=1;i<=n;i++){
@@ -21,66 +15,40 @@ void clean(){
     }
 
 }
-bool putDot(int i,int j){
 
-    for(int x=i+1;x<i+k;x++){
-        if(x>n){
-            break;
-        }
-        if(mat[x][j]=='X'){
-            return false;
-        }
-    }
-    for(int x=i-1;x>i-k;x--){
-        if(x<1){
-            break;
-        }
-        if(mat[x][j]=='X'){
-            return false;
-        }
-    }
-    for(int x=j+1;x<j+k;x++){
-        if(x>n){
-            break;
-        }
-        if(mat[i][x]=='X'){
-            return false;
-        }
-    }
-    for(int x=j-1;x>j-k;x--){
-        if(x<1){
-            break;
-        }
-        if(mat[i][x]=='X'){
-            return false;
-        }
-    }
-    return true;
-}
-
-void bfs(){
-
-    while(!marcas.empty()){
-        actual=marcas.front();
-        marcas.pop();
-        int i=actual.first,
-            j=actual.second;
+void putX(int i,int j){
+    if(i>=1 && i<=n && j>=1 && j<=n){
         mat[i][j]='X';
-        //putDot(i,j);
-        for(int k=0;k<4;k++){
-
-            if(i+pi[k]>=1 &&  i+pi[k]<=n && j+pj[k]>=1 && j+pj[k]<=n){
-                if(mat[i+pi[k]][j+pj[k]]=='.'){
-                   marcas.push(make_pair(i+pi[k],j+pj[k]));
-                }
-            }
-
-        }
-
-
+        //cont++;
     }
+}
+void mov(int x){
+    //cont=1;
+    int j=r+1;
+    for(int i=x+1;i<n+k;i++){
+        putX(j,i);
+        j++;
+    }
+    j=r-1;
+    for(int i=x-1;i>1-k;i--){
+        putX(j,i);
+        j--;
+    }
+}
+void mark(){
 
-
+    for(int x=c;x<n+n;x+=k){
+        putX(r,x);
+        //mat[r][x]='O';
+        mov(x);
+        //break;
+    }
+    for(int x=c;x>1-n;x-=k){
+        //mat[r][x]='O';
+        putX(r,x);
+        mov(x);
+        //break;
+    }
 }
 
 int main(){
@@ -90,29 +58,10 @@ int main(){
     while(t--){
 
         scanf("%d %d %d %d",&n,&k,&r,&c);
-        if(k==1){
-            for(int i=1;i<=n;i++){
-                for(int j=1;j<=n;j++){
-                    cout<<'X';
-                }
-            cout<<endl;
-            }
-            continue;
-        }
         clean();
-        pi[0]=k;pi[2]=-k;
-        pj[1]=k;pj[3]=-k;
-        marcas.push(make_pair(r,c));
-        bfs();
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=n;j++){
-                if(mat[i][j]=='.' &&  putDot(i,j)){
-                    marcas.push(make_pair(i,j));
-                    bfs();
-                }
-            }
-        }
-
+        //mat[r][c]='X';
+        mark();
+        //mat[r][c]='O';
         for(int i=1;i<=n;i++){
             for(int j=1;j<=n;j++){
                 cout<<mat[i][j];
