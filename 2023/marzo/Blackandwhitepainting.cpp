8719f342e21f317aa2b6c766cceaac16764eade9
Batishavo <romero.martinez.carlosraul@gmail.com>
2023-03-03 10:30:47 -0600

    uva blcaks and whites

--- 2023/marzo/Blackandwhitepainting.cpp
+++ 2023/marzo/Blackandwhitepainting.cpp
@@ -1,20 +1,37 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-int n,m,pos;
+int n, m;
+bool pos;
+long long cont = 0;
+int main()
+{
 
-int main(){
-
-    while(scanf("%d %d %d",&n,&m,&pos)){
-        if(n==0 && m==0 && pos==0){
+    while (scanf("%d %d %d", &n, &m, &pos))
+    {
+        if (n == 0 && m == 0 && pos == 0)
+        {
             break;
+        };
+        for (int i = 0; i < n - 7; i++)
+        {
+            //cout<<"x";
+            if (pos)
+            {
+                cont += m-7-((m - 7) / 2);
+            }
+            else
+            {
+                cont += (m - 7) / 2;
+            }
+            pos = !pos;
         }
-        pos=!pos;
-        n-=pos;
-        m-=pos;
-        printf("%d\n",(n/8)*(m/8));
+        cout << cont << endl;
+        cont = 0;
     }
 
     return 0;
 }
+// 333220842500
+// 799700028
