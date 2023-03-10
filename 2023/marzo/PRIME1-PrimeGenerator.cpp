d9ce5a608a0b6be149580581a9b470ca95ee68c7
Batishavo <romero.martinez.carlosraul@gmail.com>
2023-03-10 08:43:49 -0600

    prime generatior

--- 2023/marzo/PRIME1-PrimeGenerator.cpp
+++ 2023/marzo/PRIME1-PrimeGenerator.cpp
@@ -1,32 +1,76 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-const int limit = 1000000000+100000+10;
+const int limit = sqrt(1000000000);
 
-bool memo[limit];
+long long n,
+    m,
+    last;
 
-void criba(){
-    memo[0]=1;
-    memo[1]=1;
-    for(int i=2;i<=limit;i++){
-        if(memo[i]==0){
-            cout<<i<<",";
-            for(int j=i+i;j<=limit;j+=i){
-                memo[j]=1;
+int t;
+
+bool memo[limit + 10];
+
+vector<int> primes;
+
+void criba()
+{
+    memo[0] = 1;
+    memo[1] = 1;
+    for (int i = 2; i <= limit; i++)
+    {
+        if (memo[i] == 0)
+        {
+            // cout<<i<<",";
+            primes.push_back(i);
+            for (int j = i + i; j <= limit; j += i)
+            {
+                memo[j] = 1;
             }
         }
     }
 }
 
-int main(){
-    // freopen("entrada.txt", "r", stdin);
-    freopen("salida.txt", "w", stdout);
-    // cin>>x;
-    cout<<"[";
+int main()
+{
     criba();
-    cout<<"]\n";
-    
+    // cout<<primes.size();
+    last = primes[primes.size() - 1];
+    scanf("%d", &t);
+
+    while (t--)
+    {
+
+        cin >> n >> m;
+        for (long long i = n; i <= m; i++)
+        {
+            bool flag = 0;
+            if (i > last)
+            {
+                for (int j = 0; j < primes.size(); j++)
+                {
+                    if (i % primes[j] == 0)
+                    {
+                        flag = 1;
+                        break;
+                    }
+                }
+                if (flag == 0)
+                {
+                    cout << i << endl;
+                }
+            }
+            else
+            {
+                if (memo[i] == 0)
+                {
+                    cout << i << endl;
+                }
+            }
+        }
+        cout << endl;
+    }
 
     return 0;
 }
