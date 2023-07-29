8902983733e0e06aff83df79569d43cbfcb9ac56
Batishavo <romero.martinez.carlosraul@gmail.com>
2023-07-29 07:28:42 -0600

    cambios

--- 2023/07-Julio/27/C-Sereja_and_Swaps.cpp
+++ 2023/07-Julio/27/C-Sereja_and_Swaps.cpp
@@ -4,6 +4,8 @@ using namespace std;
 
 const int limit=210;
 
+int used[limit];
+
 int max_Sum_Sub_Array(int arr[],int n){
     
     int max_sum     = arr[0];
@@ -43,7 +45,92 @@ vector<pair<int,int>> get_positions_L_R(int arr[],int n,int max_Sum){
 
 }
 
-int m_Swap(int l,int r,int max_Sum){
+int get_Mayor(int arr[],int l,int r,int n,int &pos){
+    
+    int mayor = INT_MIN;
+    
+    for(int i=r;i<n;i++){
+        if(arr[i]>mayor && used[i]==0){
+            pos = i;
+            mayor = arr[i];
+        }
+    }
+
+    for(int i = l;i>=0;i--){
+        if(arr[i]>mayor && used[i]==0){
+            pos = i;
+            mayor = arr[i];
+        }
+    }
+
+    return mayor;
+}
+
+int m_Swap(int l,int r,int max_Sum,int k,int arr[],int n){
+
+    while(k!=0 && (l>=0 || r<n)){
+
+        int pos_mayor   = 0;
+        int pos_menor   = 0;
+        int menor       = 0;
+        int mayor       = get_Mayor(arr,l,r,n,pos_mayor);
+
+        for(int i=max(0,l);i<=min(n-1,r);i++){
+            
+            if(arr[i]<menor && used[i]==0){
+                menor     = arr[i];
+                pos_menor = i; 
+            }
+        
+        }
+
+        used[pos_menor]=1;
+
+        if(menor<0  && mayor>menor)  {
+
+            max_Sum += mayor+abs(menor);
+
+            used[pos_mayor]=1;
+        }
+        else{
+            if(mayor<=0){
+                break;
+            }
+            if(arr[l]<arr[r]){
+                l--;
+            }
+            /*else if(arr[l]==arr[r]){
+
+            }*/
+            else{
+                r++;
+            }
+
+            max_Sum+=mayor;
+
+        }
+
+        while(l>=0 && arr[l]>=0){
+            if(used[l]==0){
+                max_Sum+=arr[l];
+            }
+            l--;
+        }
+        while(r<n && arr[r]>=0){
+            if(used[r]==0){
+                max_Sum+=arr[r];
+            }
+            r++;
+        }
+
+        k--;
+    }  
+
+    for(int i=0;i<200;i++){
+        used[i]=0;
+    }
+
+    return max_Sum; 
 
 }
 
@@ -57,6 +144,7 @@ int main(){
     int arr[limit];
 
     vector<int>best_K_Numbers;
+
     vector<pair<int,int>> positions_L_R;
 
     cin>>n>>k;
@@ -73,9 +161,12 @@ int main(){
 
         answer = max(
             answer,m_Swap(
-                    positions_L_R[i].first
-                    ,positions_L_R[i].second
+                    positions_L_R[i].first-1
+                    ,positions_L_R[i].second+1
                     ,max_Sum
+                    ,k
+                    ,arr
+                    ,n
                 )
             )
         ;
