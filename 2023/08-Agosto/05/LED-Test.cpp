677b0756ce84991bab50e7757677cfe698441ed2
Batishavo <romero.martinez.carlosraul@gmail.com>
2023-08-06 10:06:42 -0600

    problemas del uva

--- 2023/08-Agosto/05/LED-Test.cpp
+++ 2023/08-Agosto/05/LED-Test.cpp
@@ -6,62 +6,81 @@ int n;
 
 string cads[15];
 
-map<string, int> led;
-map<string,bool>used;
+map<int, string> led_value;
 
-bool flag;
-
-void all_combination
-    (
-        vector<string>acumulado
-        ,string actual
-        ,int pos
-    )
+bool can_be_equals(string cad_led, string cad_tmp)
 {
 
-    if(pos==7){
-        return acumulado;
+    for (int i = 0; i < 7; i++)
+    {
+        if (cad_led[i] != cad_tmp[i] && cad_tmp[i] == 'Y')
+        {
+            return false;
+        }
     }
+    return true;
+}
 
-    for(int i=pos;i<7;i++){
-        if(actual[i]=='N'){
-            actual[i]='Y';
-            if(led[actual]!=0 && used[actual]!=0){
-                acumulado.push(actual); 
+void init()
+{
+
+    led_value[0] = "YYYYYYN";
+    led_value[1] = "NYYNNNN";
+    led_value[2] = "YYNYYNY";
+    led_value[3] = "YYYYNNY";
+    led_value[4] = "NYYNNYY";
+    led_value[5] = "YNYYNYY";
+    led_value[6] = "YNYYYYY";
+    led_value[7] = "YYYNNNN";
+    led_value[8] = "YYYYYYY";
+    led_value[9] = "YYYYNYY";
+}
+
+bool comp()
+{
+
+    int num = 9;
+
+    //cout << n<<endl;;
+
+    for (int k = 0; k <= 9 - n; k++)
+    {
+
+        int pos = 0;
+
+        bool flag = true;
+
+        for (int i = num; i > num - n; i--)
+        {
+
+            // cout << num << endl
+            //      << "*********\n";
+
+            if (!can_be_equals(led_value[i], cads[pos++]))
+            {
+                flag = false;
+                break;
             }
+        }
+
+        num--;
+
+        if (flag)
+        {
+            return true;
         }
     }
 
-}
-
-bool dp(string ante,int pos){
-
-
-
-}
-
-void comp(string cad1,string cad2){
-
-    int num1=led[cad1];
-
-    
-
-
+    return false;
 }
 
 int main()
 {
 
-    led["YYYYYYN"] = 10;
-    led["NYYNNNN"] = 1;
-    led["YYNYYNY"] = 2;
-    led["YYYYNNY"] = 3;
-    led["NYYNNYY"] = 4;
-    led["YNYYNYY"] = 5;
-    led["YNYYYYY"] = 6;
-    led["YYYNNNN"] = 7;
-    led["YYYYYYY"] = 8;
-    led["YYYYNYY"] = 9;
+    freopen("entrada.txt", "r", stdin);
+    freopen("salida.txt", "w", stdout);
+
+    init();
 
     while (cin >> n)
     {
@@ -71,23 +90,19 @@ int main()
             break;
         }
 
-        flag = false;
-
         for (int i = 0; i < n; i++)
         {
-            cin >> cad[i];
+            cin >> cads[i];
         }
 
         if (n == 1)
         {
-            cout << "MATCH";
+            cout << "MATCH\n";
         }
         else
         {
 
-            comp(cad[0],cad[n-1]);
-
-            if (flag)
+            if (comp())
             {
                 cout << "MATCH\n";
             }
