25779f5a77a537dbdc145f87c9a71c5646bda76a
batishavo <romero.martinez.carlosraul@gmail.com>
2023-09-25 14:21:25 -0600

    uva 10721 bar codes

---
+++ 2023/09-Septiembre/22/10721-Bar_Codes.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int n;
+int k;
+int m;
+
+int BC(bool ant,int tam,int sum_m,int sum_k){
+
+    if(sum_k>k || sum_m>m){
+        return 0;
+    }
+
+    if(k-sum_k>n-tam){
+        return 0;
+    }
+
+    if(tam==n ){
+        if(sum_m==m){
+            //cout<<cad<<endl;
+            return 1;
+        }
+        else{
+            return 0;
+        }
+    }
+
+    return BC(ant,tam+1,sum_m,sum_k+1) + BC(!ant,tam+1,sum_m+1,1);
+
+}
+
+int main(){
+
+    freopen("entrada.txt", "r", stdin);
+    freopen("salida.txt", "w", stdout);
+
+    while(cin>>n>>m>>k){
+        cout<<BC(1,1,1,1)<<endl;
+    }
+
+    return 0;
+}
