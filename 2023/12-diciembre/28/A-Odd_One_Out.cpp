67ba8712a90a8696b9acf125716bce6e0a6f8a27
batishavo <romero.martinez.carlosraul@gmail.com>
2023-12-30 11:45:38 -0600

    codeforces

---
+++ 2023/12-diciembre/28/A-Odd_One_Out.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int diferentNumber(int a, int b, int c)
+{
+
+    if (a == b)
+    {
+        return c;
+    }
+    else if (a == c)
+    {
+        return b;
+    }
+    else
+    {
+        return a;
+    }
+}
+
+int main()
+{
+
+    // freopen("entrada.txt", "r", stdin);
+    // freopen("salida.txt", "w", stdout);
+
+    int n;
+
+    cin >> n;
+
+    while (n--)
+    {
+        int a, b, c;
+
+        cin >> a >> b >> c;
+
+        cout << diferentNumber(a, b, c)<<endl;
+    }
+
+    return 0;
+}
---
+++ 2023/12-diciembre/29/B-Not_Quite_Latin_Square.cpp
@@ -0,0 +1,73 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string leters[5];
+
+bool isPosible(char character, int i, int j)
+{
+
+    for (int x = 0; x < 3; x++)
+    {
+        if (leters[i][x] == character)
+        {
+            return false;
+        }
+    }
+    for (int x = 0; x < 3; x++)
+    {
+        if (leters[x][j] == character)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+char misingCharacter()
+{
+
+    char misingCharacter[5] = {'A', 'B', 'C'};
+
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            if (leters[i][j] == '?')
+            {
+                for (int x = 0; x < 3; x++)
+                {
+                    if (isPosible(misingCharacter[x], i, j))
+                    {
+                        return misingCharacter[x];
+                    }
+                }
+            }
+        }
+    }
+    return '-';
+}
+
+int main()
+{
+
+    // freopen("entrada.txt", "r", stdin);
+    // freopen("salida.txt", "w", stdout);
+
+    int t;
+
+    scanf("%d", &t);
+
+    while (t--)
+    {
+
+        for (int i = 0; i < 3; i++)
+        {
+            cin >> leters[i];
+        }
+
+        cout << misingCharacter() << endl;
+    }
+
+    return 0;
+}
